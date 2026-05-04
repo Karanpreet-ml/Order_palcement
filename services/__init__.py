57ac0532b3d7b66aa1a97934c95cd4caf6c22b12
Karanpreet-ml <singhkaranpreet@seasiaconnect.com>
2026-05-04 15:35:25 +0530

    Delete services directory

--- services/__init__.py
+++
@@ -1,9 +0,0 @@
-from importlib import import_module
-
-__all__ = ["ProcurementRecommendationService"]
-
-
-def __getattr__(name):
-    if name == "ProcurementRecommendationService":
-        return import_module(".recommendation_service", __name__).ProcurementRecommendationService
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
